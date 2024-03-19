b0fd1cff947fef2b4b3168f2fbfb36ac18abcc3e
KevinTelefonica <162622063+KevinTelefonica@users.noreply.github.com>
2024-03-19 09:18:34 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+class Locators:
+    """
+        Class that contains the paths of automation
+    """
+    def __init__(self):
+        """
+            Dados iniciais
+        """
+        self.dados = 'Dados'
+
+
+class Controller:
+    """
+        class made for controll the automation
+    """
+    def __init__(self):
+        """
+            Dados Iniciais
+        """
+        self.locators = Locators()
+        print("ola")
+
+    def dados(self):
+        """
+            Printando dados iniciais
+        """
+        print(self.locators.dados)
+
+    def teste(self):
+        """
+            printando
+        """
+        print("apenas um teste")
